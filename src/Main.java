40fdc52d072950be081d2d67ff6adf2036d452b0
Den Study <study.denony@gmail.com>
2026-02-20 21:33:26 +0100

    Add things

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     static void main() {
-
+        // Пошаговая инструкция:
+//         1. Создаем файл .gitignore и добавляєем туда папку .idea
+//         2. Создаем ветку блог командой git branch blog и входим в нее git checkout blog
+//         3. Создаем файлы index.js, index.html
+//         4. Додаем все файлы в локальное хранилище git add . и затверждаем git commit -m "Add things"
+//         5. Проверка записались ли все файлы git status
+//         6.
+//
     }
 }
