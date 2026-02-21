0267d1d71335638217d01086ba40bd0651db6b36
Den Study <study.denony@gmail.com>
2026-02-21 09:45:24 +0100

    Add things

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
 //         3. Создаем файлы index.js, index.html
 //         4. Додаем все файлы в локальное хранилище git add . и затверждаем git commit -m "Add things"
 //         5. Проверка записались ли все файлы git status
-//         6.
-//
+//         6. Добавляем файл на удаленный репозиторий командой git remote add Homework6 https://github.com/studydroid123/Test.git
+//         7. Выгружаем основной файл git push -u Homework6 master
+//         8. Проверяем все ли записалось на странице github
+//         9.
     }
 }
